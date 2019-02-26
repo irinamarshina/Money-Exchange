9c8965c6e280361e7a5b2386a8ad1d29515f5919
irinamarshina <31935561+irinamarshina@users.noreply.github.com>
2019-02-26 14:45:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,31 +2,32 @@
 module.exports = function makeExchange(currency) {
 	var obj = {};
 	if(currency > 0 && currency < 10000){
-		var coin50 = coin25 = coin10 = coin5 = coin1 =0;
+		var coin50 = coin25 = coin10 = coin5 = coin1 =0, ost;
 		coin50  = Math.floor(currency / 50);
-		currency = currency - coin50 * 50;
-		coin25 = Math.floor(currency / 25);
-		currency = currency - coin25 * 25;
-		coin10 = Math.floor(currency / 10);
-		currency = currency - coin10 * 10;
-		coin5 = Math.floor(currency / 5);
-		currency = currency - coin5 * 5;
-		coin1 = currency / 1;
-		if (coin50 != 0){
+		if (coin50){
 			obj["H"]  = coin50;
 		}
-		if (coin25 != 0){
+		currency = currency % 50;
+		coin25 = Math.floor(currency / 25);
+		if (coin25){
 			obj["Q"]  = coin25;
 		}
-		if (coin10 != 0){
+		currency = currency % 25;
+		coin10 = Math.floor(currency / 10);
+		if (coin10){
 			obj["D"]  = coin10;
 		}
-		if (coin5 != 0){
+		currency = currency % 10;
+		coin5 = Math.floor(currency / 5);
+		if (coin5){
 			obj["N"]  = coin5;
 		}
-		if (coin1 != 0){
+		currency = currency % 5;
+		coin1 = currency / 1;
+		if (coin1){
 			obj["P"]  = coin1;
 		}
+		
 	}
 	else if(currency != 0 && currency > 10000){
 		obj.error = "You are rich, my friend! We don't have so much coins for exchange";
